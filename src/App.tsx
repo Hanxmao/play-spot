347de2b54d822c86e861a4ab6b92865d36040d79
Hanxmao <hanxmao@hotmail.com>
2025-04-30 15:44:40 -0700

    change theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import { NearbySportsProvider } from './context/NearbySportsContext';
 
 function App() {
   return (
-    //TODO: remove after fix
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<NearbySportsFinder />} />
-    //     <Route path="/place/:id" element={<PlaceDetail />} />
-    //   </Routes>
-    // </BrowserRouter>
     <>
       {/* TODO: Navbar place holder */}
       <div>
