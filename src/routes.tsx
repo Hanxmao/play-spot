add4c559a647a98ccef13f489f322d68bb0caee2
Hanxmao <hanxmao@hotmail.com>
2025-05-02 20:20:46 -0700

    fix place detail page naming

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App.tsx";
-import PlaceDetail from "./components/PlaceDetail.tsx";
+import PlaceDetail from "./pages/PlaceDetailPage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 
